f3f2c4a0b163e5797064a85ea12156d0dbaab9b5
Marci6 <zmarci14@gmail.com>
2017-11-18 12:40:09 +0100

    Alert dialog at delete

--- app/src/main/java/hu/bme/aut/personaltaskmanager/model/ProjectRecyclerAdapter.java
+++ app/src/main/java/hu/bme/aut/personaltaskmanager/model/ProjectRecyclerAdapter.java
@@ -1,6 +1,8 @@
 package hu.bme.aut.personaltaskmanager.model;
 
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +17,8 @@ import hu.bme.aut.personaltaskmanager.R;
 import hu.bme.aut.personaltaskmanager.ui.handling_tasks.OnProjectSelectedListener;
 
 public class ProjectRecyclerAdapter extends RecyclerView.Adapter<ProjectRecyclerAdapter.ProjectViewHolder>{
+
+    public static final int EDIT_PROJECT_REQUEST_CODE = 101;
 
     private List<Project> projects;
     private OnProjectSelectedListener listener;
@@ -64,7 +68,7 @@ public class ProjectRecyclerAdapter extends RecyclerView.Adapter<ProjectRecycler
         ImageButton editButton;
         ImageButton removeButton;
 
-        public ProjectViewHolder(View itemView) {
+        public ProjectViewHolder(final View itemView) {
             super(itemView);
 
             iconImageView = (ImageView) itemView.findViewById(R.id.ProjectFolderIconImageView);
@@ -74,16 +78,34 @@ public class ProjectRecyclerAdapter extends RecyclerView.Adapter<ProjectRecycler
             editButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-
+                    //NewProjectDialogFragment dialog = new NewProjectDialogFragment();
+                    //dialog.setTargetFragment(frag, EDIT_PROJECT_REQUEST_CODE);
+                    //dialog.show(getFragmentManager(), NewProjectDialogFragment.TAG);
                 }
             });
             removeButton = (ImageButton) itemView.findViewById(R.id.ProjectRemoveButton);
             removeButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    Project p = projects.remove(getAdapterPosition());
-                    DataManager.getInstance().removeProject(p);
-                    notifyItemRemoved(getAdapterPosition());
+                    AlertDialog.Builder adb = new AlertDialog.Builder(itemView.getContext());
+                    adb.setMessage(R.string.delete_project_alert_msg);
+                    adb.setTitle("Delete " + projects.get(getAdapterPosition()).getTitle() + " Project");
+                    adb.setIcon(android.R.drawable.ic_dialog_alert);
+                    adb.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            Project p = projects.remove(getAdapterPosition());
+                            DataManager.getInstance().removeProject(p);
+                            notifyItemRemoved(getAdapterPosition());
+                        }
+                    });
+                    adb.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            return;
+                        }
+                    });
+                    adb.show();
                 }
             });
 
